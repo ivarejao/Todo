5afcbe507be11e559dc758624b9592b013daf74d
Fernando <fernando.azevedo.peres@gmail.com>
2021-09-30 20:58:30 -0300

    Comentando MainActivity e resolvendo warnings

--- app/src/main/java/com/androidapp/todo/activities/MainActivity.kt
+++ app/src/main/java/com/androidapp/todo/activities/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.androidapp.todo.activities
 
+import android.annotation.SuppressLint
 import android.app.DatePickerDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
-import android.widget.DatePicker
 import android.widget.Toast
 import androidx.room.Room
 import com.androidapp.todo.database.TaskDatabase
@@ -16,50 +16,60 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var db : TaskDatabase
+    // Banco de dados da aplicação.
+    private lateinit var db : TaskDatabase
     private lateinit var binding: ActivityMainBinding
 
-    var cal:Calendar = Calendar.getInstance()
+    // Calendario e data como variáveis globais.
+    private var cal:Calendar = Calendar.getInstance()
     private var date : String = ""
 
+    @SuppressLint("SimpleDateFormat")
     override fun onCreate(savedInstanceState: Bundle?) {
         // Inicializa a tela principal da aplicação.
         super.onCreate(savedInstanceState)
+
+        // Fazendo binding da tela para não ter que buscar os elementos da mesma toda hora.
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
-        // Cria o banco de dados.
+        // Recupera o banco de dados.
         db = Room.databaseBuilder(applicationContext, TaskDatabase::class.java, "TaskList").build()
 
-        // Cria o evento de seleção da data.
-        val dateSetListener = object : DatePickerDialog.OnDateSetListener{
-            override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        // Evento que ocorre quando a data for selecionada dentro do dialog.
+        val dateSetListener =
+            DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
                 cal.set(Calendar.YEAR, year)
                 cal.set(Calendar.MONTH, month)
                 cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
 
+                // Faz a formatação da data lida usando o locale da Itália pois era o que mais se adaptava
+                // ao nosso sistema de data dentre os locales disponiveis.
                 val formatador = SimpleDateFormat("dd//MM/yyyy", Locale.ITALY)
 
                 date = formatador.format(cal.time)
             }
 
-        }
-
+        // Criando o handler para quando o botão 'DATA' for clicado.
         binding.date.setOnClickListener{
+            // Cria um dialog para a seleção da data.
             DatePickerDialog(this@MainActivity, dateSetListener,
                 cal.get(Calendar.YEAR),
                 cal.get(Calendar.MONTH),
                 cal.get(Calendar.DAY_OF_MONTH)).show()
         }
 
+        // Criando o handler para quando o botão 'CADASTRAR' for clicado.
         binding.register.setOnClickListener{
 
+            // Recuperação dos valores digitados nos campos.
             val title = binding.title.text.toString()
             val subtitle = binding.subtitle.text.toString()
             val text = binding.text.text.toString()
             val sync = binding.checkBox.isChecked
 
+            // Tratamento das variáveis lidas.
             when {
                 title.trim().isEmpty() -> {
                     Toast.makeText(this, "Título da tarefa não pode estar em branco!", Toast.LENGTH_SHORT).show()
@@ -78,39 +88,48 @@ class MainActivity : AppCompatActivity() {
                     return@setOnClickListener
                 }
                 else -> {
+                    // Caso as variáveis estejam todas preenchidas, executa este código
                     val format = SimpleDateFormat("dd//MM/yyyy")
 
+                    // Cria uma instancia da classe tarefa.
                     val task = Task(
                         0,
                         title,
                         subtitle,
                         text,
-                        format.parse(date.toString()),
+                        format.parse(date),
                         sync
                     )
 
+                    // Insere o objeto no banco de dados e limpa a tela.
+                    // Dentro de uma thread para não atrapalhar o usuário.
                     Thread{
                         db.TaskDao().insertTasks(task)
                         runOnUiThread {
-                            val str = ""
-                            binding.title.text = createEditable(str)
-                            binding.subtitle.text = createEditable(str)
-                            binding.text.text = createEditable(str)
+                            binding.title.text = createEditable()
+                            binding.subtitle.text = createEditable()
+                            binding.text.text = createEditable()
                             date = ""
                             if (sync)
                                 binding.checkBox.toggle()
                         }
                     }.start()
+
+                    //
+                    // TODO
+                    // Caso sync seja true interagir com a API google calendar
+                    //
                 }
             }
         }
 
+        // Criando o handler para quando o botão 'LISTAR' for clicado
         binding.list.setOnClickListener{
+            // Muda a tela para a activity List
             val intent = Intent(applicationContext, List::class.java)
             startActivity(intent)
         }
     }
 
-    private fun createEditable(text : String) = Editable.Factory.getInstance().newEditable(text)
-
+    private fun createEditable() = Editable.Factory.getInstance().newEditable("")
 }
